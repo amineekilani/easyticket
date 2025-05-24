683341488edff00054d089db81a36f558bd77a96
Amine Kilani <aminekilani901@gmail.com>
2025-05-24 22:14:00 +0100

    Annulation de billets

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Repository\BilletRepository;
 use App\Repository\CommandeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class TicketController extends AbstractController
 {
@@ -76,5 +78,45 @@ class TicketController extends AbstractController
         return $this->render('customer/commandes/show.html.twig', [
             'commande' => $commande,
         ]);
+    }
+    
+    #[Route('/ticket/{id}/cancel', name: 'app_ticket_cancel', methods: ['POST'])]
+    public function cancel(string $id, BilletRepository $billetRepository, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+        
+        $billet = $billetRepository->find($id);
+        
+        if (!$billet || $billet->getCommande()->getUser() !== $user) {
+            throw $this->createNotFoundException('Billet non trouvé');
+        }
+        
+        // Vérifier si le match est dans plus de 24 heures
+        $matchDateTime = $billet->getMatch()->getDateEtHeure();
+        $now = new \DateTime();
+        $timeBeforeMatch = $matchDateTime->getTimestamp() - $now->getTimestamp();
+        
+        if ($timeBeforeMatch <= 86400) { // 86400 seconds = 24 hours
+            $this->addFlash('error', 'L\'annulation n\'est possible que 24 heures avant le match.');
+            return $this->redirectToRoute('app_tickets');
+        }
+        
+        // Logique d'annulation (à personnaliser selon vos besoins)
+        // Par exemple, marquer le billet comme annulé ou le supprimer
+        
+        // Option 1: Supprimer le billet
+        $entityManager->remove($billet);
+        
+        // Option 2: Ou le marquer comme annulé (si vous avez un champ status dans votre entité Billet)
+        // $billet->setStatus('annulé');
+        
+        $entityManager->flush();
+        
+        $this->addFlash('success', 'Votre billet a été annulé avec succès.');
+        
+        return $this->redirectToRoute('app_tickets');
     }
 }

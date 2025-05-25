cae5d025c5193de8c220c07bea6eff5dd33f2fb3
Baha Manai <70824369+BahaManai@users.noreply.github.com>
2025-05-25 02:31:01 +0100

    correct authentification problems

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,21 +14,32 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/set-flash-message', name: 'app_set_flash_message', methods: ['POST'])]
+    public function setFlashMessage(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $type = $data['type'] ?? 'info';
+        $message = $data['message'] ?? 'Message non spécifié';
+
+        $this->addFlash($type, $message);
+
+        return new JsonResponse(['status' => 'success']);
     }
 }
